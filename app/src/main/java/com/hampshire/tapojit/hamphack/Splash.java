e2829d39d48adbd34476f98e156bc0a321a4f060
Tapojit <tapojitdebnath@gmail.com>
2017-04-17 18:31:40 -0400

    Update Splash.java

--- app/src/main/java/com/hampshire/tapojit/hamphack/Splash.java
+++ app/src/main/java/com/hampshire/tapojit/hamphack/Splash.java
@@ -28,30 +28,5 @@ public class Splash extends Activity {
         };
         c1.start();
 
-//        final ImageView iv=(ImageView) findViewById(R.id.spl);
-//        final Animation an= AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
-//        final Animation an2=AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
-//
-//        iv.startAnimation(an);
-//        an.setAnimationListener(new Animation.AnimationListener() {
-//            @Override
-//            public void onAnimationStart(Animation animation) {
-//
-//            }
-//
-//            @Override
-//            public void onAnimationEnd(Animation animation) {
-//                iv.startAnimation(an2);
-//                finish();
-//                Intent i=new Intent(getApplicationContext(), MainActivity.class);
-//                startActivity(i);
-//
-//            }
-//
-//            @Override
-//            public void onAnimationRepeat(Animation animation) {
-//
-//            }
-//        });
     }
 }
